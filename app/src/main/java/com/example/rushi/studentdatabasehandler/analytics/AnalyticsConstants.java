4331dff2ff74e72a27377c3872dc60c5c366d2c6
Rushi Thakker <rushi.thakker@jeavio.com>
2019-02-22 18:36:35 +0530

    Latest code after codelabs

--- app/src/main/java/com/example/rushi/studentdatabasehandler/analytics/AnalyticsConstants.java
+++ app/src/main/java/com/example/rushi/studentdatabasehandler/analytics/AnalyticsConstants.java
@@ -4,6 +4,17 @@ import com.example.rushi.studentdatabasehandler.BuildConfig;
 
 public class AnalyticsConstants
 {
+  /**
+   * This constant is used to identify which analytics tool shall be used. These would be useful
+   * when we want to compare firebase and flurry. For demo purpose, I shall be going through experience booking flow
+   * and logging events and user properties for the same.
+   *  1 - Firebase
+   *  2 - Facebook
+   *  3 - Both tools
+   */
+  private static final int  ANALYTICS_TOOL= 3;
+  public static final int isDebugAnalyticsOn = BuildConfig.DEBUG?ANALYTICS_TOOL:-1;
+
   public static class CommonConstants
   {
     public static  class Event
@@ -17,16 +28,4 @@ public class AnalyticsConstants
       public static final String COLOR = "color";
     }
   }
-
-
-  /**
-   * This constant is used to identify which analytics tool shall be used. These would be useful
-   * when we want to compare firebase and flurry. For demo purpose, I shall be going through experience booking flow
-   * and logging events and user properties for the same.
-   *  1 - Firebase
-   *  2 - Facebook
-   *  3 - Both tools
-   */
-  private static final int  ANALYTICS_TOOL= 3;
-  public static final int isDebugAnalyticsOn = BuildConfig.DEBUG?ANALYTICS_TOOL:-1;
 }
--- app/src/main/java/com/example/rushi/studentdatabasehandler/analytics/AnalyticsUtility.java
+++ app/src/main/java/com/example/rushi/studentdatabasehandler/analytics/AnalyticsUtility.java
@@ -1,10 +1,5 @@
 package com.example.rushi.studentdatabasehandler.analytics;
 
-import com.example.rushi.studentdatabasehandler.App;
-import com.example.rushi.studentdatabasehandler.analytics.firebaseanalyticsutility.FAUtility;
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import static com.example.rushi.studentdatabasehandler.App.getFirebaseAnalytics;
 import static com.example.rushi.studentdatabasehandler.App.getFirebaseAnalyticsUtility;
 
 public class AnalyticsUtility
